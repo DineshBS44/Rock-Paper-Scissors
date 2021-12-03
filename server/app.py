3ae206ab8de967845ff23a152e02ac9876e19141
Dinesh B S <dineshbs444@gmail.com>
2021-12-03 19:27:45 +0530

    Added api endpoint to handle the image

--- server/app.py
+++ server/app.py
@@ -2,20 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import tensorflow as tf
-import tensorflow_datasets as tfds
-
 from tensorflow import keras
 
 from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    image = request.args.get('image')
+@app.route("/api/predict", methods=["POST"])
+def predict():
+    file = request.file
+    image = file.get('image')
     InputData = [[image]]
     loaded_model = keras.models.load_model('./my_model')
     Output = loaded_model.predict(InputData)
     Output = '{ "Prediction": "' + str(Output[0]) + '" }'
 
-    return Output
+    return jsonify({
+        'Prediction': str(Output[0])
+    })
+
+@app.route("/")
+def index():
+    return "Not a valid endpoint"
