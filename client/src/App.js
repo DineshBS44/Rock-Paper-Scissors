8ec3ddcdf7be502fe294c44cb3c7cdc550b96703
Dinesh B S <dineshbs444@gmail.com>
2021-12-03 23:02:27 +0530

    Frontend setup and tested

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,91 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button, Form} from 'semantic-ui-react';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+      displayError: "",
+      displayResult: "",
+    };
+  }
+
+  onSubmitHandler = (event) => {
+    event.preventDefault();
+    
+    if(this.state.image != null) {
+      const formData = new FormData();
+      formData.append('image', this.state.image);
+      // fetch('http://localhost:5000/api/predict', {
+      //   method: 'POST',
+      //   body: formData
+      // })
+      // .then(response => response.json())
+      // .then(response => {
+      //   if (response.error) {
+      //     this.setState({
+      //       displayError: response.error,
+      //       displayResult: "",
+      //     });
+      //   } else {
+      //     this.setState({
+      //       displayError: "",
+      //       displayResult: response.Prediction,
+      //     });
+      //   }
+      // });
+
+      console.log("Executing this");
+      axios.post('http://localhost:5000/api/predict', formData)
+      .then(response => {
+        if (response.data.error) {
+          this.setState({
+            displayError: response.data.error,
+            displayResult: "",
+          });
+        } else {
+          this.setState({
+            displayError: "",
+            displayResult: response.data.Prediction,
+          });
+        }
+      });
+    }
+
+  };
+
+  onFileChange = event => {
+    this.setState({ image: event.target.files[0] });  
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Rock, Paper and Scissors ML Prediction</h1>
+        <br />
+        <h3>Upload an image below to see the prediction</h3>
+        <br />
+        <Form onSubmit={this.onSubmitHandler}>
+        <Form.Field>
+        <input type="file" accept=".png" onChange={this.onFileChange}></input>
+        </Form.Field>
+        <br />
+        <Button primary>Upload!</Button>
+        </Form>
+        <br />
+        <br />
+        <p>{this.state.displayError}</p>
+        <br />
+        <br />
+        <p> The result of the prediction is: {this.state.displayResult}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
