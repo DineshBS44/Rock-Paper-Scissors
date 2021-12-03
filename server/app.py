94d3cb58c4028b2ca6010d012d1abf81be6959e5
Dinesh B S <dineshbs444@gmail.com>
2021-12-03 22:10:17 +0530

    Debug and Test API Post Endpoint for prediction

--- server/app.py
+++ server/app.py
@@ -1,25 +1,53 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
 
 import tensorflow as tf
 from tensorflow import keras
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/api/predict", methods=["POST"])
 def predict():
-    file = request.file
-    image = file.get('image')
-    InputData = [[image]]
+    imgStr = request.files["image"].read()
+    npimage = np.fromstring(imgStr, np.uint8)
+    image = cv2.imdecode(npimage, cv2.IMREAD_GRAYSCALE)
+
+    InputData = np.array(image)
+    print(InputData.shape)
+    print(type(InputData))
+
+    InputData = InputData.reshape(1, 300, 300, 1)
+    InputData = InputData.astype('float32')
+    InputData /= 255
+    print(InputData.shape)
+
     loaded_model = keras.models.load_model('./my_model')
     Output = loaded_model.predict(InputData)
-    Output = '{ "Prediction": "' + str(Output[0]) + '" }'
+
+    print("Output: ", Output)
+    print(type(Output[0][0]))
+
+    mostProbable = 0.0
+    prediction = "None"
+    for i in range(3):
+        if float(Output[0][i]) > mostProbable:
+            mostProbable = float(Output[0][i])
+            if i == 0:
+                prediction = "Rock"
+            elif i == 1:
+                prediction = "Paper"
+            else:
+                prediction = "Scissors"
+    
+    print("Prediction: ", prediction)
 
     return jsonify({
-        'Prediction': str(Output[0])
+        'Prediction': prediction
     })
+
 
 @app.route("/")
 def index():
